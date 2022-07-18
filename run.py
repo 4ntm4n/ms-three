1ef029bfc9b99cb5378c44cf8041f96a594c84c3
Anton Askling <anton.askling@gmail.com>
2022-07-18 19:10:57 +0200

    outline functions of battle ship games add function to create and  print out the board.

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+def ship_location():
+    return [[" "] * 5 for x in range(5)]
+
+hidden_board = ship_location()
+guessing_board = ship_location()
+
+
+def print_board(board): 
+    print ("    A B C D E")
+    print("   -----------")
+    row_number = 1
+    for row in board:
+        print("{}  |{}|".format(row_number,"|".join(row)))
+        row_number += 1
+
+
+print_board(hidden_board)
+
+def get_ship_location():
+    pass
+
+def count_hits():
+    pass
+
+def create_ships():
+    pass
+
+    turns = 10
+    #while turns > 0:
