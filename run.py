46725df9aa733c37fe8dc022c8e08c97e4fbacd4
Anton Askling <anton.askling@gmail.com>
2022-07-19 02:24:20 +0200

    add print function to print out playing board to console.

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
-board = []
+# list that holds the character that represents the board in the terminal
+board = [["."] * 5 for times in range(0,5)]
 
+
+def print_board(board):
+    row_count = 1
+    print("   A B C D E")
+    for row in board:
+        print(f"{row_count}  " +" ".join(row))
+        row_count += 1
+
+
+print_board(board)
