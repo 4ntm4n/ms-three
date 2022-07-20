afff64682478246d158005012c279ec0e4fb8cd4
Anton Askling <anton.askling@gmail.com>
2022-07-20 18:56:28 +0200

    STARTING OVER add class for board and start to define methods in the board class

--- run.py
+++ run.py
@@ -1,13 +1,38 @@
 # list that holds the character that represents the board in the terminal
-board = [["."] * 5 for times in range(0,5)]
+from random import randint
+
+class BoardMaker:
+    def __init__(self, owner, type):
+        """
+        Initializing a playing board
+        """
+        self.owner = owner
+        self.board = [["."] * 5 for times in range(5)]
+        self.type = type
+
+    def create_cordinate()
+        pass
+
+    def place_ships(self, cordinate:
+        pass
+
+    def print_board(self):
+        """
+        Method that prints the board to the console.
+        """
+        print(f"{self.owner}'s board: \n")
+        print("   A B C D E")
+        row_count = 1
+        for row in self.board:
+             print(f"{row_count}  " + " ".join(row))
+             row_count += 1
+             
 
 
-def print_board(board):
-    row_count = 1
-    print("   A B C D E")
-    for row in board:
-        print(f"{row_count}  " +" ".join(row))
-        row_count += 1
 
 
-print_board(board)
+
+player_board = BoardMaker("Player", "human")
+computer_board = BoardMaker("Computer", "computer")
+
+computer_board.print_board()
