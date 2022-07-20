89018c09d1f306607936290137feabe93679d5ab
Anton Askling <anton.askling@gmail.com>
2022-07-21 01:52:36 +0200

    add logic to evaluate if ship was hit or missed.

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from random import randint
 
 class BoardMaker:
-    def __init__(self, owner, type):
+    def __init__(self, name, type):
         """
         Initializing a playing board
         """
-        self.owner = owner
+        self.name = name
         self.board = [["."] * 5 for times in range(5)]
         self.type = type
         self.ship_locations = []
@@ -27,7 +27,7 @@ class BoardMaker:
         """
         Method that prints the board to the console.
         """
-        print(f"\n{self.owner}'s board: \n")
+        print(f"\n{self.name}'s board: \n")
         print("   A B C D E")
         row_count = 1
         for row in self.board:
@@ -49,8 +49,8 @@ class Player(BoardMaker):
     Player has access to the board made in BoardMaker, a guess method and
     that collects user input and stores old guesses.
     """
-    def __init__(self, owner, type):
-        super().__init__(owner, type)
+    def __init__(self, name, type):
+        super().__init__(name, type)
     
     def guess(self):
         """
@@ -61,18 +61,26 @@ class Player(BoardMaker):
         letters = "abcde"
 
         row = input("select a row (1-5)")
-        while row == "" and row not in numbers:
+        while row not in numbers:
             print("cmon man, you missed the ocean, 1-5 please!")
             row = input("select a row (1-5)")
 
         col = input("select a column (A-E)").lower()
-        while col == "" and col not in letters:
+        while col not in letters:
             print("cmon man, you missed the ocean, A-E please!")
             col = input("select a column (A-E)").lower()
         
-        guess = (row, col)
-        self.guesses.append(guess)
+        guess = (int(row), letters.index(col))
         return guess
+    
+    def update_guesses(self, guess):     
+        self.guesses.append(guess)   
+    
+    def miss(self, guess):
+        self.board[guess[0]][guess[1]] = "0"
+
+    def hit(self, guess):
+        self.board[guess[0]][guess[1]] = "x"           
 
 
 class ArtificialPlayer(BoardMaker):
@@ -81,21 +89,44 @@ class ArtificialPlayer(BoardMaker):
     Artificial player has access to the board, makes random guesses and stores
     previous guesses.
     """
-    def __init__(self, owner, type):
-        super().__init__(owner, type)
+    def __init__(self, name, type):
+        super().__init__(name, type)
 
-    def random_guess(self):
+    def guess(self):
         guess = (randint(0,4), randint(0,4))
-        self.guesses.append(guess)
         return guess
+
+    def update_guesses(self):     
+        self.guesses.append(self.guess)
+
+    def miss(self, guess):
+        self.board[guess[0]][guess[1]] = "0"
+
+    def hit(self, guess):
+        self.board[guess[0]][guess[1]] = "x"    
 
 
 def check_for_hit(player, opponent):
-    pg = player.guess()
+    """
+    Function that evaluates a guess made from either player1 or player2.
+    """
+    print(player.guesses)
+    guess = player.guess()
+    print(guess)
+    if guess in player.guesses:
+        print("you have already tried that")
+        check_for_hit(player, opponent)
 
-    if pg in opponent.locations:
-        pass
+    if guess in opponent.ship_locations:
+        opponent.hit(guess)
+        player.update_guesses(guess)
+        print(f"hit! one of {opponent.name}'s ship has sunk.")
+        opponent.ship_locations.remove(guess)
 
+    else:
+        opponent.miss(guess)
+        player.update_guesses(guess)
+        print("miss...") 
 
 def play(player1, player2):
     """
@@ -103,17 +134,13 @@ def play(player1, player2):
     """
     player.print_board()
     computer.print_board()
-    while player1.ship_locations != [] or player2.ship_locations != []:
-        pass
-
-        #check_for_hits()
-
-
+    check_for_hit(player1, player2)
 
 
 #create instance of players.
 player = Player("Anton", "human")
 computer = ArtificialPlayer("Computer", "computer")
+
 
 #place ships on board.
 computer.place_ships()
@@ -121,9 +148,10 @@ player.place_ships()
 
 #visual representation of the ships
 player.reveal_ships()
-
 #load gameplay
 play(player, computer)
+
+
 #player_board = BoardMaker("Player", "human")
 #computer_board = BoardMaker("Computer", "computer")
 
